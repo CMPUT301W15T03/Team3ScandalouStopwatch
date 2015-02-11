7ff4b2efbd7e8e19dcf9ce9c2b5001e039967ec1
jlhostyn <jlhostyn@ualberta.ca>
2015-02-11 12:18:58 -0700

    added empty tests for use cases 4

---
+++ ScandalouTravelTracker/ScandalouTravelTrackerTest/src/ca/ualberta/cs/scandaloutraveltracker/test/ExpenseOverallTests.java
@@ -0,0 +1,45 @@
+package ca.ualberta.cs.scandaloutraveltracker.test;
+
+import junit.framework.TestCase;
+
+public class ExpenseOverallTests extends TestCase {
+	//use case UC 04.01.01 
+	public void testCreateExpense() {
+		
+	}
+	
+	//use case UC 04.02.01 
+	public void testExpenseCategories() {
+		
+	}
+	
+	//use case UC 04.03.01 
+	public void testExpenseCurrency() {
+		
+	}
+	
+	//use case UC 04.04.01 
+	public void testExpenseFlag() {
+		
+	}
+	
+	//use case UC 04.05.01 - 04.05.02
+	public void testExpenseDetails() {
+		
+	}
+	
+	//use case UC 04.06.01
+	public void testExpenseEdit() {
+		
+	}
+	
+	//use case UC 04.07.01 
+	public void testExpenseDelete() {
+		
+	}
+	
+	//use case UC 04.08.01 
+	public void testExpenseNavigation() {
+		
+	}
+}
