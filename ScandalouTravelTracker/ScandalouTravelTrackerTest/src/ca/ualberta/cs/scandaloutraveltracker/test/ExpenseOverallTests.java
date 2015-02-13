204ec232934afe61af9a544bde6cba47c66ec15a
jlhostyn <jlhostyn@ualberta.ca>
2015-02-13 13:01:38 -0700

    Added License

--- ScandalouTravelTracker/ScandalouTravelTrackerTest/src/ca/ualberta/cs/scandaloutraveltracker/test/ExpenseOverallTests.java
+++ ScandalouTravelTracker/ScandalouTravelTrackerTest/src/ca/ualberta/cs/scandaloutraveltracker/test/ExpenseOverallTests.java
@@ -1,3 +1,21 @@
+/*
+
+Copyright 2015 Team3ScandalouStopwatch
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
 package ca.ualberta.cs.scandaloutraveltracker.test;
 
 import java.util.Arrays;
@@ -105,7 +123,7 @@ public class ExpenseOverallTests extends TestCase {
 		
 		Expense testExpense = new Expense(date, category, description, cost, currencyType);
 		//should return string of testExpense that will display all info for expense list
-		String details = testExpense.getDetails();
+		String details = testExpense.toString();
 		//can change format
 		String actualDetails = "Air Fare - 01/01/2014" + "\nFlight to YEG" + "\n566 CAD";
 		assertTrue("The details aren't being returned properly for displaying", details.equals(actualDetails));
@@ -156,8 +174,9 @@ public class ExpenseOverallTests extends TestCase {
 	}
 	
 	//Test for MinNavigationAddExpense
-	//Test if navigation to add an expense is 2 clicks
+	//Test if navigation to add an expense is 2-3 screen/clicks
 	public void testExpenseNavigation() {
-		//manually test if expense add screen can be added in 2 clicks when app starts up
+		//manually test if expense add screen can be added in 2-3 screens/clicks when app starts up
+		//when UI made will make intent test
 	}
 }
