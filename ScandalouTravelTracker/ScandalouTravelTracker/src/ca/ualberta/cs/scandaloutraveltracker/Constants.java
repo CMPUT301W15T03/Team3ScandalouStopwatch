de91bf109da2fff8d90a492e1bafa6f4ac9c99c6
thousand-fjords <tlazar@ualberta.ca>
2015-03-10 17:29:50 -0600

    Created class for global constants

---
+++ ScandalouTravelTracker/ScandalouTravelTracker/src/ca/ualberta/cs/scandaloutraveltracker/Constants.java
@@ -0,0 +1,8 @@
+package ca.ualberta.cs.scandaloutraveltracker;
+
+public class Constants {
+
+	// Making this mm/dd/yyyy for now; feel free to change it
+	public final static String dateFormat = "mm/dd/yyyy";
+	
+}
