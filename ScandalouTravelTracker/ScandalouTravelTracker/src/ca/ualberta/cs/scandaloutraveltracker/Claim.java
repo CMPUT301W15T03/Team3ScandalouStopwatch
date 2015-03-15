e6f0227c55f9cb6f22ee048b6a129b103bedfcd7
datjwu <justin.h.y.wu@gmail.com>
2015-03-15 11:50:26 -0600

    Started Javadoc in Documentation folder 	Only done for Claim.java so far

--- ScandalouTravelTracker/ScandalouTravelTracker/src/ca/ualberta/cs/scandaloutraveltracker/Claim.java
+++ ScandalouTravelTracker/ScandalouTravelTracker/src/ca/ualberta/cs/scandaloutraveltracker/Claim.java
@@ -16,28 +16,22 @@ limitations under the License.
 
 */
 
-/* Claim.java Basic Info:
- *  Class that contains the Claim model. Edits to any instance of a Claim
- *  should be done through the ClaimController class.
- */
-
 package ca.ualberta.cs.scandaloutraveltracker;
 
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Locale;
 
-import android.content.Context;
-
+/**
+ *  Class that contains the Claim model. Edits to any instance of a Claim
+ *  should be done through the ClaimController class.
+ * @author Team3ScandalouStopwatch
+ */
 public class Claim extends SModel implements Comparable<Claim> {
 	
 	private int id;
-	
-	private Context context;
 	
 	private String name;
 	private String description;
@@ -46,13 +40,22 @@ public class Claim extends SModel implements Comparable<Claim> {
 	private ArrayList<Expense> expenses;
 	private ArrayList<Destination> destinations;
 	private ArrayList<String> tags; 
-	private HashMap<String, Double> totals;
 	private Boolean canEdit;
 	private String status;
 	private String approverName;
 	private String approverComment;
-	private User user;
+	// private User user;
 
+	/**
+	 * Fetches a claim corresponding to the id passed to it. Uses
+	 * the ClaimMapper class to retrieve the saved claim. 
+	 *
+	 * @param id    The id of the claim. This is created whenever a
+	 *              new Claim is created and is how the ClaimMapper
+	 *              class knows which claim is which when saved.
+	 * @see ClaimMapper
+	 */
+	@SuppressWarnings("unchecked")
 	public Claim(int id){
 		ClaimMapper mapper = new ClaimMapper(ClaimApplication.getContext());
 
@@ -67,22 +70,21 @@ public class Claim extends SModel implements Comparable<Claim> {
 		this.approverName = (String)mapper.loadClaimData(id, "approverName");
 		this.approverComment = (String)mapper.loadClaimData(id, "approverComment");
 		this.expenses = (ArrayList<Expense>)mapper.loadClaimData(id, "expenses");
-		// Still need: totals
 		this.canEdit = (Boolean)mapper.loadClaimData(id, "canEdit");
 	}
 	
-	// Constructor to make a more complete claim (feel free to add parameters as needed)
-	public Claim(String name, String description, Date sDate, Date eDate, 
-			ArrayList<Destination> destinations, ArrayList<String> tags) {
-		this.name = name;
-		this.description = description;
-		this.startDate = sDate;
-		this.endDate = eDate;
-		this.destinations = destinations;
-		this.tags = tags;
+	/**
+	 * Constructor that is used to create claims quickly for
+	 * testing the claims.
+	 *
+	 */
+	public Claim() {
 		this.canEdit = true;
 		this.expenses = new ArrayList<Expense>();
-	}	
+		this.destinations = new ArrayList<Destination>();
+		this.tags = new ArrayList<String>();
+		this.status = "In Progress";
+	}
 	
 	// Constructor to quickly make a claim
 	public Claim(String name, Date sDate, Date eDate) {
@@ -92,118 +94,243 @@ public class Claim extends SModel implements Comparable<Claim> {
 		this.canEdit = true;
 	}
 	
-	public Claim() {
-		// Constructor for empty claim to test
-		this.canEdit = true;
-		this.expenses = new ArrayList<Expense>();
-		this.destinations = new ArrayList<Destination>();
-		this.tags = new ArrayList<String>();
-		this.status = "In Progress";
-	}
-	
 	// Getter and Setter Methods
 	
+	/**
+	 * @return Claim id
+	 */
 	public int getId() {
 		return id;
 	}
+	
+	/**
+	 * 
+	 * @param id
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}	
 	
+	/**
+	 * 
+	 * @return Claimant's name
+	 */
 	public String getName() {
 		return name;
 	}
+	
+	/**
+	 * Currently incomplete. Should set the name to the name
+	 * associated with user and not have a setName method
+	 * within the Claim class.
+	 * @param name Claimant's name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	
+	/**
+	 * 
+	 * @return Claim's description
+	 */
 	public String getDescription() {
 		return description;
 	}
+	
+	/**
+	 * 
+	 * @param description
+	 */
 	public void setDescription(String description) {
 		this.description = description;
 	}
 
-	
+	/**
+	 * 
+	 * @return Start date for the claim
+	 */
 	public Date getStartDate() {
 		return startDate;
 	}
-	public String getStartDateString() {
-		SimpleDateFormat sdf = new SimpleDateFormat(Constants.dateFormat, Locale.US);
-		return sdf.format(this.startDate);
-	}
+	
+	/**
+	 * 
+	 * @param startDate
+	 */
 	public void setStartDate(Date startDate) {
 		this.startDate = startDate;
 	}
 	
+	/**
+	 * 
+	 * @return Start date for the claim as a string
+	 */
+	public String getStartDateString() {
+		SimpleDateFormat sdf = new SimpleDateFormat(Constants.dateFormat, Locale.US);
+		return sdf.format(this.startDate);
+	}
+	
+	/**
+	 * 
+	 * @return End date for the claim
+	 */
 	public Date getEndDate() {
 		return endDate;
 	}
+	
+	/**
+	 * 
+	 * @param endDate
+	 */
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	/**
+	 * 
+	 * @return End date for the claim as a string
+	 */
 	public String getEndDateString() {
 		SimpleDateFormat sdf = new SimpleDateFormat(Constants.dateFormat, Locale.US);
 		return sdf.format(this.endDate);
 	}
-	public void setEndDate(Date endDate) {
-		this.endDate = endDate;
-	}
 
+	/**
+	 * 
+	 * @return ArrayList of destinations associated with the claim
+	 */
 	public ArrayList<Destination> getDestinations() {
 		return destinations;
 	}
+	
+	/**
+	 * 
+	 * @param destinations
+	 */
 	public void setDestinations(ArrayList<Destination> destinations) {
 		this.destinations = destinations;
 	}	
 	
+	/**
+	 * 
+	 * @return ArrayList of expenses associated with the claim
+	 */
 	public ArrayList<Expense> getExpenses() {
 		return expenses;
 	}
+	
+	/**
+	 * 
+	 * @param expenses
+	 */
 	public void setExpenses(ArrayList<Expense> expenses) {
 		this.expenses = expenses;
 	}
 	
+	/**
+	 * 
+	 * @return ArrayList of strings called tags that are associated
+	 * 		   with the claim.
+	 */
 	public ArrayList<String> getTags() {
 		return tags;
 	}
+	
+	/**
+	 * 
+	 * @param tags
+	 */
 	public void setTags(ArrayList<String> tags) {
 		this.tags = tags;
 	}
 	
-	public HashMap<String, Double> getTotals() {
-		return totals;
-	}
-	public void setTotals(HashMap<String, Double> totals) {
-		this.totals = totals;
-	}
-	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getStatus(){
 		return status;
 	}
+	
+	/**
+	 * 
+	 * @param status
+	 */
 	public void setStatus(String status) {
 		this.status = status;
 	}
 
+	/**
+	 * 
+	 * @return Name of the user that approved the claim
+	 */
 	public String getApproverName(){
 		return approverName;
 	}
+	
+	/**
+	 * 
+	 * @param approverName
+	 */
 	public void setApproverName(String approverName) {
 	}
 
+	/**
+	 * 
+	 * @return Comment that the approver left on the claim
+	 */
 	public String getApproverComment(){
 		return approverComment;
 	}
+	
+	/**
+	 * 
+	 * @param approverComment
+	 */
 	public void setApproverComment(String approverComment) {
+		this.approverComment = approverComment;
 	}	
 	
-	public void setCanEdit(boolean b) {
-		this.canEdit = b;
+	/**
+	 * 
+	 * @param canEdit
+	 */
+	public void setCanEdit(boolean canEdit) {
+		this.canEdit = canEdit;
 	}
 	
+	/**
+	 * 
+	 * @return A boolean to see if the claim can be edited.
+	 */
 	public boolean getCanEdit() {
 		return this.canEdit;
 	}
 	
+	/**
+	 * Returns an expense in the list of expenses associated
+	 * with the position passed.
+	 * @param position
+	 * @return Expense located in position
+	 */
+	public Expense getExpense(int position) {
+		return expenses.get(position);
+	}
+	
 	// Modification methods
 	
+	/**
+	 * The updateClaim method takes any of the fields that can
+	 * be changed by a claimant and updates the claim by using
+	 * the claim's id along with a claimMapper.
+	 * 
+	 * @param startDate
+	 * @param endDate
+	 * @param description
+	 * @param destinations
+	 * @param canEdit
+	 * @see ClaimMapper
+	 */
 	public void updateClaim(Date startDate, Date endDate, String description,
 			ArrayList<Destination> destinations, boolean canEdit){
 		
@@ -220,6 +347,13 @@ public class Claim extends SModel implements Comparable<Claim> {
 		notifyViews();
 	}
 	
+	/**
+	 * Updates the list of tags associated with the claim. 
+	 * Uses the ClaimMapper to save the updated tag values
+	 * and notifies the views associated with the Claim to
+	 * update.
+	 * @param tags
+	 */
 	public void updateTags(ArrayList<String> tags){
 		
 		setTags(tags);
@@ -230,6 +364,15 @@ public class Claim extends SModel implements Comparable<Claim> {
 		notifyViews();
 	}
 	
+	/**
+	 * Updates the Claim to the properties that are
+	 * associated with a submitted claim. This is, setting
+	 * the status to submitted and canEdit boolean to false.
+	 * Uses the ClaimMapper to save updated data.
+	 * @param status
+	 * @param canEdit
+	 * @see ClaimMapper
+	 */
 	public void submitClaim(String status, boolean canEdit){
 		
 		ClaimMapper mapper = new ClaimMapper(ClaimApplication.getContext());
@@ -238,18 +381,34 @@ public class Claim extends SModel implements Comparable<Claim> {
 		notifyViews();
 	}
 	
+	/**
+	 * 
+	 * @param destination
+	 */
 	public void addDestination(Destination destination) {
 		this.destinations.add(destination);
 	}
 
+	/**
+	 * 
+	 * @param destination
+	 */
 	public void removeDestination(Destination destination) {
 		this.destinations.remove(destination);
 	}
 	
+	/**
+	 * 
+	 * @param expense
+	 */
 	public void addExpense(Expense expense) {
 		this.expenses.add(expense);
 	}
 	
+	/**
+	 * 
+	 * @param expense
+	 */
 	public void deleteExpense(Expense expense) {
 		this.expenses.remove(expense);
 		notifyViews();
@@ -257,9 +416,12 @@ public class Claim extends SModel implements Comparable<Claim> {
 	
 	// String Conversion Methods
 	
-	// Didn't change anything in this method; just commenting it out for now so that
-	// newly created claims don't break the list (since they don't have expenses yet)
-
+	/**
+	 * Uses a HashMap to calculate the total cost of the claim. 
+	 * The string value is associated with the currency and the
+	 * double is associated with the cost.
+	 * @return HashMap with totaled costs from expenses
+	 */
 	public HashMap<String, Double> computeTotal() {
 		
 		HashMap<String, Double> totals = new HashMap<String, Double>();
@@ -282,9 +444,12 @@ public class Claim extends SModel implements Comparable<Claim> {
 		return totals;
 	}
 
-	
-	// Converts the destinations into a string
-	// String format is destination1, destination2, ..., destination x 
+	/**
+	 * Converts all the destinations associated with a claim
+	 * into a string. The format of this string is:
+	 * destination1, destination2, ..., destination x
+	 * @return A string with the Claim's destination names
+	 */
 	public String destinationsToString() {
 		String destinations = "";
 		int count = 1;
@@ -304,33 +469,50 @@ public class Claim extends SModel implements Comparable<Claim> {
 		return destinations;
 	}
 	
-	// Warning Methods
-	
-	public String getWarning(){
-		return null;
-	}
-	
-	public void raiseWarning(String warning){
-		// TODO: Do something
-	}
-	
 	// Methods for the Approver Only
+	
+	/**
+	 * Sets the claim as approved and sets the claim's approver
+	 * name. 
+	 * Currently incomplete. Should get the approverName from
+	 * user. Perhaps pass in the Approver rather than String.
+	 * @param approverName
+	 */
 	public void approveClaim(String approverName){
 		this.status = "Approved";
+		this.approverName = approverName;
 		this.canEdit = false;
 	}
 	
+	/**
+	 * Sets the claim as returned and sets the claim's approver
+	 * name.
+	 * Currently incomplete. Should get the approverName from
+	 * user. Perhaps pass in the Approver rather than String.
+	 * @param approverName
+	 */
 	public void returnClaim(String approverName){
 		this.status = "Returned";
+		this.approverName = approverName;
 		this.canEdit = true; 
 	}
 
-	// Method for comparing claims to one another (comparing dates)
+	/**
+	 * 
+	 * @param another A second claim to be compared to
+	 * @return 0 if dates equal, >0 if date is after another, <0 if date is before another
+	 */
 	@Override
 	public int compareTo(Claim another) {
 		return another.getStartDate().compareTo(startDate);
 	}
 
+	/**
+	 * Takes all the tags associated with the claim and
+	 * prints them out as a string in the format:
+	 * tag1, tag2, tag3, ..., tag x
+	 * @return String that displays tags
+	 */
 	public String tagsToString() {
 		String tags = "";
 		int counter = 1;
@@ -348,11 +530,6 @@ public class Claim extends SModel implements Comparable<Claim> {
 		}
 		
 		return tags;
-	}
-
-	public Expense getExpense(int position) {
-		// TODO Auto-generated method stub
-		return expenses.get(position);
 	}
 
 }
