1f3379976ec283b2d9717f58408eaf29f52b0b30
thousand-fjords <tlazar@ualberta.ca>
2015-03-16 12:12:33 -0600

    Added/updated license headers

--- ScandalouTravelTracker/ScandalouTravelTrackerTest/src/ca/ualberta/cs/scandaloutraveltracker/test/ClaimsApprovalTest.java
+++ ScandalouTravelTracker/ScandalouTravelTrackerTest/src/ca/ualberta/cs/scandaloutraveltracker/test/ClaimsApprovalTest.java
@@ -1,6 +1,6 @@
 /*
 
-Copyright 2015 ScandalouStopwatch
+Copyright 2015 Team3ScandalouStopwatch
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
