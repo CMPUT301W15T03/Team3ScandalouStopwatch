ee96b0bf46f717e8c536f30a77d80802061aaa72
jlhostyn <jlhostyn@ualberta.ca>
2015-04-01 13:35:23 -0600

    added license to every java file that didn't have our license

--- ScandalouTravelTracker/ScandalouTravelTracker/src/ca/ualberta/cs/scandaloutraveltracker/views/ViewLocationActivity.java
+++ ScandalouTravelTracker/ScandalouTravelTracker/src/ca/ualberta/cs/scandaloutraveltracker/views/ViewLocationActivity.java
@@ -1,3 +1,21 @@
+/*
+
+Copyright 2015 Team3ScandalouStopwatch
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+*/
+
 package ca.ualberta.cs.scandaloutraveltracker.views;
 
 import org.osmdroid.api.IMapController;
@@ -42,8 +60,6 @@ public class ViewLocationActivity extends MenuActivity {
         lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
         locationTV = (TextView) findViewById(R.id.view_location_text_view);
         
-        
-      //sets start point to current expense location, gps location if available, else it will default to around Edmonton
         Intent intent = getIntent();
         previousLocation = new Location("Expense Location");
         previousLocation.setLatitude(intent.getDoubleExtra("latitude", 999));
